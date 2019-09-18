8b53dabf515deb78818d55790d284988ea1affbc
shuvokr <shuvo@Shuvos-Air.mshome.net>
2019-09-18 12:10:10 +0600

    Added script for create framework Build and store framework

---
+++ iOS_Swift_Framework/AlamoWater/AlamoWater.framework/Headers/AlamoWater.h
@@ -0,0 +1,19 @@
+//
+//  AlamoWater.h
+//  AlamoWater
+//
+//  Created by Shuvo on 18/9/19.
+//  Copyright © 2019 com.example.learn.iOS.swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamoWater.
+FOUNDATION_EXPORT double AlamoWaterVersionNumber;
+
+//! Project version string for AlamoWater.
+FOUNDATION_EXPORT const unsigned char AlamoWaterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamoWater/PublicHeader.h>
+
+
