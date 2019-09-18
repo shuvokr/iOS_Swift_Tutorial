d42e5d53373f1f6f4c21a4eabd3973662f8daa64
shuvokr <shuvo@Shuvos-MacBook-Air.local>
2019-09-18 11:14:14 +0600

    Created a new NSObject class named AlamoWater

---
+++ iOS_Swift_Framework/Framewowrk_test/AlamoWater/AlamoWater/AlamoWater.swift
@@ -0,0 +1,13 @@
+//
+//  AlamoWater.swift
+//  AlamoWater
+//
+//  Created by Shuvo on 18/9/19.
+//  Copyright © 2019 com.example.learn.iOS.swift. All rights reserved.
+//
+
+import UIKit
+
+class AlamoWater: NSObject {
+
+}
